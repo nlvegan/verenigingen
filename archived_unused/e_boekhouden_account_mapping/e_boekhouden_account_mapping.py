abbff7fb6b731f00f0c0f55ec5a559c8d8ec5a96
Your Name <your.email@example.com>
2025-08-25 20:07:18 +0200

    removed unused code

--- archived_unused/e_boekhouden_account_mapping/e_boekhouden_account_mapping.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2025, Frappe Technologies and contributors
-# For license information, please see license.txt
-
-from frappe.model.document import Document
-
-
-class EBoekhoudenAccountMapping(Document):
-    pass
